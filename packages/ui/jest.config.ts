a0b26950720aff5006c25dbf20cb06b11ab832de
Ferdi Ardiansa <ferdiardiansa@gmail.com>
2026-05-19 02:33:19 +0700

    chore(monorepo): extract shared jest config into @clera/config-jest

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -1,38 +1,12 @@
 import type { Config } from 'jest'
+import { baseConfig } from '@clera/config-jest'
 
 const config: Config = {
-  preset: 'ts-jest/presets/default-esm',
-
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-
+  ...baseConfig,
   moduleNameMapper: {
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
     '\\.svg$': '<rootDir>/__mocks__/svg.ts',
     '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(' +
-      'gsap' +
-      '|@radix-ui/.*' +
-      '|@hookform/.*' +
-      '|@tanstack/.*' +
-      '|cmdk' +
-      '|react-icons' +
-      '|date-fns' +
-      '|zod' +
-      ')/)',
-  ],
-
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      useESM: true,
-      tsconfig: 'tsconfig.json'
-    }]
   },
 }
 
